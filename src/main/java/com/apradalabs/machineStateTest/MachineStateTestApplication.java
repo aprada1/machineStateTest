6a7208a7aecf68f74b8aad89a43f7f37cd78f741
aprada <andres209@gmail.com>
2024-08-06 11:02:02 -0500

    firts commit

---
+++ src/main/java/com/apradalabs/machineStateTest/MachineStateTestApplication.java
@@ -0,0 +1,13 @@
+package com.apradalabs.machineStateTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineStateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineStateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apradalabs/machineStateTest/MachineStateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apradalabs.machineStateTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineStateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
